063c429ab35f6f97b2607bb5b2d5182c82e29676
Steefyo <33659399+Steefyo@users.noreply.github.com>
2020-10-27 14:08:19 +0100

    Update Opdracht_8.ino

--- Arduino/Opdracht_8/Opdracht_8.ino
+++ Arduino/Opdracht_8/Opdracht_8.ino
@@ -1,124 +1,126 @@
-// Gebruik Library
-#include <IRremote.h>
+// Gebruiken van een library
+#include<IRremote.h>
 
-// Knop IR code.
-#define button_1 38108
-#define button_2 29937
-#define button_3 22976
-#define button_4 34528
+// Afstandingsbediening resultaten opslaan
+decode_results resultaten;
 
-// Definieer pins.
-int receiver_pin = 5;
-int led_1 = 12;
-int led_2 = 10;
-int led_3 = 8;
-int led_4 = 7;
+// Tel hoeveel lampen er aanstaan.
+int lampOnCount = 0;
 
-// Definieer een opslag voor de lamp logica.
-int led[] = {0,0,0,0};
-int timer[] = {100, 200, 300, 400};
+// Defineer de lampen aan.
+int ledAan1 = 0;
+int ledAan2 = 0;
 
-// Zet reciever klaar.
-IRrecv receiver(receiver_pin);
+// Knipper tijd
+unsigned long blinkerTime = 0;
 
-// Verwerk de uitkomst.
-decode_results output;
+// Defineer pins van de leds
+int ledPins[] = {13, 12, 10, 9};
 
-// Start Serial Monitor
-// Start de reciever
-// Zet de gedefinieerde pins open.
+// Defineer afstandsbediening
+int IR = 5;
+IRrecv irrecv(IR);
+
+// Toetsen op de afstandsbediening
+long int knop_1 = 16;
+long int knop_2 = 2064;
+long int knop_3 = 1040;
+long int knop_4 = 3088;
+
+// Zet elke pin aan de lamp
+// Serial monitor
+// IR ontvanger aanzetten
 void setup() {
+  for (int i = 0; i > 4; i++) {
+    pinMode(ledPins[0], OUTPUT);
+  }
   Serial.begin(9600);
-  receiver.enableIRIn();
-  pinMode(led_1, OUTPUT);
-  pinMode(led_2, OUTPUT);
-  pinMode(led_3, OUTPUT);
-  pinMode(led_4, OUTPUT);
+  irrecv.enableIRIn();
 }
 
 void loop() {
-  // Ontvang de IR signalen.
-  if (receiver.decode(&output)) {
-    // Sla het signaal op.
-    unsigned int value = output.value;
-    // Kijk wel signaal het is.
-    switch(value) {
-      // Zet per knop de lamp aan/uit en zet boolean
-      case button_1:
-        if(led[0] == 1) {
-          digitalWrite(led_1, LOW);
-          led[0] = 0;
-        } else {
-          led[0] = 1;
-        }
-        break;
-      case button_2:
-        if(led[1] == 1) {
-          digitalWrite(led_2, LOW);
-          led[1] = 0;
-        } else {
-          led[1] = 1;
-        }
-        break;
-      case button_3:
-        if(led[2] == 1) {
-          digitalWrite(led_3, LOW);
-          led[2] = 0;
-        } else {
-          led[2] = 1;
-        }
-        break;
-      case button_4:
-        if(led[3] == 1) {
-          digitalWrite(led_4, LOW);
-          led[3] = 0;
-        } else {
-          led[3] = 1;
-        }
-        break;
+  // Lees de lamp uit
+  int statusLed1 = digitalRead(ledPins[0]);
+  int statusLed2 = digitalRead(ledPins[1]);
+  int statusLed3 = digitalRead(ledPins[2]);
+  int statusLed4 = digitalRead(ledPins[3]);
+
+  // Lees IR uit in resultaten
+  if (irrecv.decode(&resultaten)) {
+    // Toon welke is ingetoetst.
+    Serial.println(resultaten.value);
+    // Ga verder met ontvangen.
+    irrecv.resume();
+    // Als er minder dan 2 lampen aanstaan
+    if (lampOnCount < 2) {
+      // Op lamp ...
+      // Zet lamp ... in de functie
+      if (resultaten.value == knop_1 && statusLed1 == LOW) {
+        onthoudLed(ledPins[0]);
+      } else if (resultaten.value == knop_2 && statusLed2 == LOW) {
+        onthoudLed(ledPins[1]);
+      } else if (resultaten.value == knop_3 && statusLed3 == LOW) {
+        onthoudLed(ledPins[2]);
+      } else if (resultaten.value == knop_4 && statusLed4 == LOW) {
+        onthoudLed(ledPins[3]);
+      }
     }
-
-    // Toon in Serial Monitor.
-    // Wacht op het volgende signaal.
-    Serial.println(value);
-    receiver.resume();
   }
 
-  // Zet de lamp aan/uit op basis van de invoer. (incl timer)
-  if (led[0] == 0 && led[1] == 0 && led[2] == 0 && led[3] == 0) {
-    digitalWrite(led_2, HIGH);
-    digitalWrite(led_3, HIGH);
-    delay(timer[2]);
-    digitalWrite(led_2, LOW);
-    digitalWrite(led_3, LOW);
-    delay(timer[2]);
+  // Als er 2 lampen aanstaan
+  if (lampOnCount == 2) {
+    // Welke knippertijd wordt het
+    // 0.1 sec / 0.2 sec / 0.3 sec / 0.4 sec
+    if (resultaten.value == knop_1) {
+      zetLedKnipperTijd(100);
+    }
+    if (resultaten.value == knop_2) {
+      zetLedKnipperTijd(200);
+    }
+    if (resultaten.value == knop_3) {
+      zetLedKnipperTijd(300);
+    }
+    if (resultaten.value == knop_4) {
+      zetLedKnipperTijd(400);
+    }
   }
-  
-  if (led[0] == 1) {
-    digitalWrite(led_1, HIGH);
-    delay(timer[0]);
-    digitalWrite(led_1, LOW);
-    delay(timer[0]);
+}
+
+void onthoudLed(int ledPin) {
+  // Zet lamp aan
+  digitalWrite(ledPin, HIGH);
+  // Als lamp uit is zet aan
+  //    Sla op welke lamp aan staat.
+  //    Aantal lampen aan +1
+  // Anders zet de andere lamp aan
+  if (ledAan1 == 0) {
+    ledAan1 = ledPin;
+    lampOnCount++;
+  } else {
+    ledAan2 = ledPin;
+    lampOnCount++;
   }
-  
-  if (led[1] == 1) {
-    digitalWrite(led_2, HIGH);
-    delay(timer[1]);
-    digitalWrite(led_2, LOW);
-    delay(timer[1]);
-  }
-  
-  if (led[2] ==1) {
-    digitalWrite(led_3, HIGH);
-    delay(timer[2]);
-    digitalWrite(led_3, LOW);
-    delay(timer[2]);
-  }
-  
-  if (led[3] == 1) {
-    digitalWrite(led_4, HIGH);
-    delay(timer[3]);
-    digitalWrite(led_4, LOW);
-    delay(timer[3]);
+}
+
+void zetLedKnipperTijd(int tijd) {
+  // Huidige tijd
+  unsigned long tijdNu = millis();
+  // Lees de lampen uit
+  int statusLamp1 = digitalRead(ledAan1);
+  int statusLamp2 = digitalRead(ledAan2);
+  // Vergelijk de tijden met elkaar
+  // Nieuwe tijd is groter
+  // Update deze tijd
+  if (tijdNu - blinkerTime >= tijd) {
+    blinkerTime = tijdNu;
+    if (statusLamp1 == LOW && statusLamp2 == LOW) {
+      // Zet beide lampen aan
+      digitalWrite(statusLamp1, HIGH);
+      digitalWrite(statusLamp2, HIGH);
+    } else {
+      // Zet beide lampen uit
+      digitalWrite(statusLamp1, LOW);
+      digitalWrite(statusLamp2, LOW);
+    }
   }
 }
